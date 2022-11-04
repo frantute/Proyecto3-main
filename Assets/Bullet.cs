f2d0fe360aa83b89aff80409b326c24e40c1e550
frantute <102159238+frantute@users.noreply.github.com>
2022-11-04 14:43:14 -0300

    Initial commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    public int bulletDamage;
+    bool colision = false;
+    public LayerMask whatIsEnemies;
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void Update()
+    {
+        transform.position += transform.up * speed;
+        
+        if (colision == true)
+        {
+            RecibirDaño();
+        }
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            RecibirDaño();
+        }
+    }
+
+    private void RecibirDaño()
+    {
+        Collider[] enemies = Physics.OverlapSphere(transform.position, whatIsEnemies);
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i].GetComponent<Enemigo>().TakeDamage(bulletDamage);
+        }
+
+        Invoke("Delay", 0.05f);
+    }
+}
---
+++ Assets/Enemigo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    public float enemyHealth = 100;
+    int enemyCount = 0;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        enemyHealth -= damage;
+
+        if (enemyHealth <= 0)
+        {
+            Invoke(nameof(DestroyEnemy), 0.2f);
+        }
+    }
+
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+        enemyCount++;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "FPSController")
+        {
+            Debug.Log("Zombie toca al player");
+            animator.SetTrigger("Pegar");
+        }
+    }
+}
